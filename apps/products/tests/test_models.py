1292215e8415d6f846f0c03aa713c3318e28f0f1
Manuel Jesús De la Cruz Sotelo <manuel7851@gmail.com>
2018-04-30 07:42:39 -0500

    FIX delete unused tests and new namespaces and names to urls

--- apps/products/tests/test_models.py
+++ apps/products/tests/test_models.py
@@ -11,7 +11,7 @@ from model_mommy import mommy
 
 
 # Local imports
-from ..models import Image, Kind, Product
+from ..models import Kind, Product
 
 
 # Create your model tests here.
@@ -22,11 +22,6 @@ class KindTestCase(TestCase):
     def test_method_str_return_name_and_category(self):
         self.assertTrue(self.kind.name in self.kind.__str__())
         self.assertTrue(self.kind.category in self.kind.__str__())
-
-    def test_there_should_not_be_two_types_with_the_same_name(self):
-        kind_name = self.kind.name
-        with self.assertRaises(ValidationError):
-            Kind.objects.create(name=kind_name)
 
     def test_have_queryset_products(self):
         self.assertTrue(self.kind.products.model is Product)
@@ -41,9 +36,6 @@ class ProductTestCase(TestCase):
 
     def test_method_str_return_name(self):
         self.assertEqual(self.product.__str__(), self.product.name)
-
-    def test_have_queryset_images(self):
-        self.assertTrue(self.product.images.model is Image)
 
     def tearDown(self):
         self.product.delete()
--- apps/products/tests/test_viewsets.py
+++ apps/products/tests/test_viewsets.py
@@ -16,10 +16,32 @@ from rest_framework.test import APITestCase
 # Create your viewset tests here.
 class KindTests(APITestCase):
     def setUp(self):
-        self.visits_url = reverse("api:kind-list")
+        self.visits_url = reverse("api_v1:kinds-list")
 
     def test_get_kind_list(self):
         response = self.client.get(self.visits_url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    # def tearDown(self):
+
+
+class MostViewedTests(APITestCase):
+    def setUp(self):
+        self.most_visits_url = reverse("api_v1:most_viewed-list")
+
+    def test_get_response(self):
+        response = self.client.get(self.most_visits_url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    # def tearDown(self):
+
+
+class NewersTests(APITestCase):
+    def setUp(self):
+        self.newers_url = reverse("api_v1:newers-list")
+
+    def test_get_response(self):
+        response = self.client.get(self.newers_url)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
     # def tearDown(self):
