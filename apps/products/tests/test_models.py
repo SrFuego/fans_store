5cc215cf2316dfe83c3c47f115c0ad4e2232808d
Manuel Jesús De la Cruz Sotelo <manuel7851@gmail.com>
2018-04-30 08:24:43 -0500

    ADD tests

--- apps/products/tests/test_models.py
+++ apps/products/tests/test_models.py
@@ -11,10 +11,21 @@ from model_mommy import mommy
 
 
 # Local imports
-from ..models import Kind, Product
+from ..models import Color, Kind, Model, Product, Size
 
 
 # Create your model tests here.
+class ColorTestCase(TestCase):
+    def setUp(self):
+        self.color = mommy.make(Color)
+
+    def test_method_str_return_name_and_category(self):
+        self.assertTrue(self.color.name in self.color.__str__())
+
+    def tearDown(self):
+        self.color.delete()
+
+
 class KindTestCase(TestCase):
     def setUp(self):
         self.kind = mommy.make(Kind)
@@ -37,5 +48,19 @@ class ProductTestCase(TestCase):
     def test_method_str_return_name(self):
         self.assertEqual(self.product.__str__(), self.product.name)
 
+    def test_have_queryset_models(self):
+        self.assertTrue(self.product.models.model is Model)
+
     def tearDown(self):
         self.product.delete()
+
+
+class SizeTestCase(TestCase):
+    def setUp(self):
+        self.size = mommy.make(Size)
+
+    def test_method_str_return_name_and_category(self):
+        self.assertTrue(self.size.name in self.size.__str__())
+
+    def tearDown(self):
+        self.size.delete()
--- apps/products/tests/test_viewsets.py
+++ apps/products/tests/test_viewsets.py
@@ -6,11 +6,13 @@ from django.urls import reverse
 
 
 # Third party apps imports
+from model_mommy import mommy
 from rest_framework import status
 from rest_framework.test import APITestCase
 
 
 # Local imports
+from ..models import Product
 
 
 # Create your viewset tests here.
@@ -28,20 +30,34 @@ class KindTests(APITestCase):
 class MostViewedTests(APITestCase):
     def setUp(self):
         self.most_visits_url = reverse("api_v1:most_viewed-list")
+        self.products = mommy.make(Product, _quantity=6)
 
     def test_get_response(self):
         response = self.client.get(self.most_visits_url)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
-    # def tearDown(self):
+    def test_get_6_elements(self):
+        response = self.client.get(self.most_visits_url)
+        self.assertEqual(len(response.data), Product.objects.all().count())
+
+    def tearDown(self):
+        for product in self.products:
+            product.delete()
 
 
 class NewersTests(APITestCase):
     def setUp(self):
         self.newers_url = reverse("api_v1:newers-list")
+        self.products = mommy.make(Product, _quantity=6)
 
     def test_get_response(self):
         response = self.client.get(self.newers_url)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
-    # def tearDown(self):
+    def test_get_6_elements(self):
+        response = self.client.get(self.newers_url)
+        self.assertEqual(len(response.data), Product.objects.all().count())
+
+    def tearDown(self):
+        for product in self.products:
+            product.delete()
