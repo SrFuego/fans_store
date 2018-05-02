8186bbeae80bec1a9e46d83c9ff0a144f5ed0774
Manuel Jesús De la Cruz Sotelo <manuel7851@gmail.com>
2018-05-01 19:59:51 -0500

    ADD test to 100% coverage

--- apps/orders/tests/test_models.py
+++ apps/orders/tests/test_models.py
@@ -6,9 +6,20 @@ from django.test import TestCase
 
 
 # Third party apps imports
+from model_mommy import mommy
 
 
 # Local imports
+from ..models import Order
 
 
 # Create your model tests here.
+class OrderTestCase(TestCase):
+    def setUp(self):
+        self.order = mommy.make(Order)
+
+    def test_method_str_return_name_and_category(self):
+        self.assertTrue(self.order.name in self.order.__str__())
+
+    def tearDown(self):
+        self.order.delete()
