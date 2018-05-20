fbc40092d3a84a687c6d5f90935056ec1884e523
Manuel Jesús De la Cruz Sotelo <manuel7851@gmail.com>
2018-05-20 18:02:24 -0500

    ADD export clients and orders

--- apps/orders/admin.py
+++ apps/orders/admin.py
@@ -7,18 +7,24 @@ from django.contrib import admin
 
 # Third party apps imports
 from import_export.admin import ExportMixin
+from import_export.formats import base_formats
 
 
 # Local imports
 from .models import Client, Order
-from .resources import OrderResource
+from .resources import ClientResource, OrderResource
 
 
 # Register your models here.
+@admin.register(Client)
+class ClientModelAdmin(ExportMixin, admin.ModelAdmin):
+    formats = (base_formats.XLS,)
+    list_display = ("full_name", "cellphone", "email")
+    resource_class = ClientResource
+
+
 @admin.register(Order)
 class OrderModelAdmin(ExportMixin, admin.ModelAdmin):
+    formats = (base_formats.XLS,)
+    list_display = ("client", "mount", "delivered",)
     resource_class = OrderResource
-    list_display = ("id",)
-
-
-admin.site.register(Client)
--- apps/orders/resources.py
+++ apps/orders/resources.py
@@ -10,18 +10,38 @@ from import_export.resources import ModelResource
 
 
 # Local imports
-from .models import Order
+from .models import Client, Order
 
 
 # Create your resources here.
+class ClientResource(ModelResource):
+    cellphone = Field(attribute="cellphone", column_name="Celular")
+    email = Field(attribute="email", column_name="Correo")
+    full_name = Field(attribute="full_name", column_name="Nombres")
+
+    class Meta:
+        model = Client
+        fields = ("cellphone", "email", "full_name",)
+        export_order = ("full_name", "cellphone", "email",)
+
+
 class OrderResource(ModelResource):
-    full_name = Field(attribute="client__full_name", column_name="Nombres")
-    email = Field(attribute="client__email", column_name="Correo")
     cellphone = Field(attribute="client__cellphone", column_name="Celular")
-    delivered = Field(attribute="delivered", column_name="Entregado")
-    mount = Field(attribute="mount", column_name="Total")
+    delivered = Field(column_name="Entregado")
+    email = Field(attribute="client__email", column_name="Correo")
+    full_name = Field(attribute="client__full_name", column_name="Nombres")
+    model = Field(column_name="Modelos")
+    mount = Field(attribute="mount", column_name="Monto")
 
     class Meta:
         model = Order
         fields = (
-            "full_name", "email", "cellphone", "model", "delivered", "mount",)
+            "cellphone", "delivered", "email", "full_name", "model", "mount",)
+        export_order = (
+            "full_name", "cellphone", "email", "model", "delivered", "mount",)
+
+    def dehydrate_delivered(self, order):
+        return "Si" if order.delivered else "No"
+
+    def dehydrate_model(self, order):
+        return "\n".join([model.__str__() for model in order.model.all()])
